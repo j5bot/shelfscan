2c4d25221d295dd3f6d23ee4d6cf216f4954099e
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2025-05-29 18:18:49 -0400

    removed some commented out breakpoint detectors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,19 +49,6 @@ export default function RootLayout({
               <div className="z-0 relative">
                   {children}
               </div>
-
-              {/*<div className="w-2 h-2 sm:w-0 sm:h-0 md:w-0 md:h-0 lg:w-0 lg:h-0 xl:w-0 xl:h-0 2xl:w-0 2xl:h-0 absolute top-0 left-0"*/}
-              {/*     id="mobile-breakpoint-detect" />*/}
-              {/*<div className="w-0 h-0 sm:w-2 sm:h-2 md:w-0 md:h-0 lg:w-0 lg:h-0 xl:w-0 xl:h-0 2xl:w-0 2xl:h-0 absolute top-0 left-0"*/}
-              {/*     id="sm-breakpoint-detect" />*/}
-              {/*<div className="w-0 h-0 sm:w-0 sm:h-0 md:w-2 md:h-2 lg:w-0 lg:h-0 xl:w-0 xl:h-0 2xl:w-0 2xl:h-0 absolute top-0 left-0"*/}
-              {/*     id="md-breakpoint-detect" />*/}
-              {/*<div className="w-0 h-0 sm:w-0 sm:h-0 md:w-0 md:h-0 lg:w-2 lg:h-2 xl:w-0 xl:h-0 2xl:w-0 2xl:h-0 absolute top-0 left-0"*/}
-              {/*     id="lg-breakpoint-detect" />*/}
-              {/*<div className="w-0 h-0 sm:w-0 sm:h-0 md:w-0 md:h-0 lg:w-0 lg:h-0 xl:w-2 xl:h-2 2xl:w-0 2xl:h-0 absolute top-0 left-0"*/}
-              {/*     id="xl-breakpoint-detect" />*/}
-              {/*<div className="w-0 h-0 sm:w-0 sm:h-0 md:w-0 md:h-0 lg:w-0 lg:h-0 xl:w-0 xl:h-0 2xl:w-2 2xl:h-2 absolute top-0 left-0"*/}
-              {/*     id="2xl-breakpoint-detect" />*/}
           </body>
       </html>
   );
