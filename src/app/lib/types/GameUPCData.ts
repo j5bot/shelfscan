2f08ee119bc451f4e4af0d7ea35e3d61f4f64ad3
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2025-05-30 18:12:33 -0400

    towards showing more gameupc data

--- src/app/lib/types/GameUPCData.ts
+++ src/app/lib/types/GameUPCData.ts
@@ -1,11 +1,18 @@
-export type GameUPCVersionStatus = 'verified' | 'none' | 'choose_from_versions' | 'choose_from_versions_or_search';
+export type GameUPCVersionStatus = 'verified' | 'none' | 'choose_from_versions' | 'choose_from_bgg_info_or_search' | 'choose_from_versions_or_search';
+export const GameUPCVersionStatus: Record<GameUPCVersionStatus, GameUPCVersionStatus> = {
+    verified: 'verified',
+    none: 'none',
+    choose_from_versions: 'choose_from_versions',
+    choose_from_bgg_info_or_search: 'choose_from_bgg_info_or_search',
+    choose_from_versions_or_search: 'choose_from_versions_or_search',
+};
 
 export type GameUPCData = {
     status: 'ok' | 'error';
     upc: string;
     name: string;
     searched_for: string;
-    bgg_status: GameUPCVersionStatus;
+    bgg_info_status: GameUPCVersionStatus;
     bgg_info: GameUPCBggInfo[];
 };
 
--- src/app/ui/games/Scanlist.tsx
+++ src/app/ui/games/Scanlist.tsx
@@ -1,6 +1,8 @@
-import { GameUPCData } from '@/app/lib/types/GameUPCData';
+import { GameUPCData, GameUPCVersionStatus } from '@/app/lib/types/GameUPCData';
 import { getImageSizeFromUrl } from '@/app/lib/utils/image';
 import Image from 'next/image';
+import { ReactNode } from 'react';
+import { FaCheckCircle, FaQuestionCircle, FaSearch, FaSearchPlus } from 'react-icons/fa';
 import { FaBarcode, FaQuestion } from 'react-icons/fa6';
 
 type ScanlistProps = {
@@ -12,7 +14,10 @@ export function Scanlist(props: ScanlistProps) {
     const { codes, gameUPCResults } = props;
 
     return (<ul className="grid gap-2 grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg-grid-cols-6">{codes.map(code => {
-        const { bgg_info: bggInfo } = gameUPCResults[code] ?? {};
+        const {
+            bgg_info: bggInfo,
+            bgg_info_status: bggInfoStatus,
+        } = gameUPCResults[code] ?? {};
         if (!bggInfo) {
             return;
         }
@@ -29,7 +34,39 @@ export function Scanlist(props: ScanlistProps) {
             height: `${smallSquareSize}px`,
         };
 
-        return <li className="rounded-md bg-orange-100" key={code}>
+        let statusIcon: ReactNode;
+        let overlayIcon: ReactNode;
+
+        const statusIconClassName = 'w-3 h-3 md:w-5 md:h-5';
+        const overlayIconSize = (smallSquareSize ?? 100) * 0.5;
+
+        switch (bggInfoStatus) {
+            case GameUPCVersionStatus.verified:
+                statusIcon = <FaCheckCircle className={statusIconClassName} title="VerifiGameUPCVersionStatused" />;
+                break;
+            case GameUPCVersionStatus.none:
+                statusIcon = <FaQuestionCircle className={statusIconClassName} title="Unknown" />;
+                overlayIcon = <FaSearchPlus title="Must Search" color="white" size={overlayIconSize} />
+                break;
+            case GameUPCVersionStatus.choose_from_versions:
+                statusIcon = <FaSearch className={statusIconClassName} title="Choose From Versions" />;
+                overlayIcon = <FaSearch title="Must Search" color="white" size={overlayIconSize} />
+                break;
+            case GameUPCVersionStatus.choose_from_bgg_info_or_search:
+                statusIcon = <FaSearchPlus className={statusIconClassName} title="Choose or Search" />;
+                overlayIcon = <FaSearchPlus title="Must Search" color="white" size={overlayIconSize} />
+                break;
+            case GameUPCVersionStatus.choose_from_versions_or_search:
+                statusIcon = <FaSearchPlus className={statusIconClassName} title="Choose or Search" />;
+                overlayIcon = <FaSearchPlus title="Must Search" color="white" size={overlayIconSize} />
+                break;
+            default:
+                statusIcon = <FaQuestionCircle className={statusIconClassName} title="Unknown" />;
+                break;
+        }
+
+        return <li className="relative rounded-md bg-orange-100" key={code}>
+            <div className="absolute bottom-1 right-1">{statusIcon}</div>
             <div className="flex flex-col p-2 md:p-4">
                 <div className="flex justify-center items-center gap-1">
                     <FaBarcode title={code} />
@@ -37,7 +74,15 @@ export function Scanlist(props: ScanlistProps) {
                 </div>
                 {thumbnailUrl ? (
                     <div className="flex justify-center p-1">
-                        <div className="bg-orange-50 flex justify-center items-center rounded-md overflow-clip hover:overflow-visible hover:scale-150 hover:border-orange-400 hover:border" style={imageContainerStyles}>
+                        <div className={`
+                            relative
+                            bg-orange-50
+                            flex justify-center items-center
+                            rounded-md overflow-clip
+                            focus:overflow-visible focus:scale-150 focus:border-orange-400 focus:border
+                            hover:overflow-visible hover:scale-150 hover:border-orange-400 hover:border`}
+                            style={imageContainerStyles}>
+                            {/*<div className={`absolute top-1 left-1 opacity-65`}>{overlayIcon}</div>*/}
                             <Image
                                 src={thumbnailUrl}
                                 alt={name}
