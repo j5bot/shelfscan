add8d5d594998982c53da344ca69f5941fae6911
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2025-07-18 09:59:33 -0400

    plugin manager! and settings dialog!

--- src/app/lib/database/database.ts
+++ src/app/lib/database/database.ts
@@ -6,10 +6,7 @@ export type SettingEntity = {
     value: string | string[];
 };
 
-export type PluginEntity = {
-    id: string;
-    plugin: ShelfScanPlugin;
-};
+export type PluginEntity = ShelfScanPlugin;
 
 export const database = new Dexie('db') as Dexie & {
     settings: EntityTable<SettingEntity, 'id'>;
@@ -25,29 +22,4 @@ export const getSetting = async (id: string) =>
     (await database.settings.get(id))?.value;
 
 export const getPlugin = async (id: string) =>
-    (await database.plugins.get(id))?.plugin;
-
-export const addPlugin = async (pluginJSON: string) => {
-    const plugin = JSON.parse(pluginJSON);
-    const id = plugin.id;
-
-    const pluginListSetting = await database.settings.get('plugins');
-    if (!pluginListSetting) {
-        await database.settings.add({ id: 'plugins', value: [id] });
-    } else {
-        await database.settings.put({ id: 'plugins', value: [...pluginListSetting.value, id]});
-    }
-    try {
-        await database.plugins.add(plugin);
-    } catch (e) {
-        await database.plugins.put(plugin);
-    }
-};
-
-export const getEnabledPlugins = async (): Promise<ShelfScanPlugin[]> => {
-    const pluginList = (await getSetting('plugins') as string[]) ?? [];
-    return (await Promise
-        .all(
-            pluginList.map(pluginId => getPlugin(pluginId))
-        )).filter((x: unknown) => !!x) as ShelfScanPlugin[];
-};
+    (await database.plugins.get(id));
--- src/app/ui/plugins/PluginManager.tsx
+++ src/app/ui/plugins/PluginManager.tsx
@@ -1,9 +1,8 @@
-import { addPlugin } from '@/app/lib/database/database';
 import { PluginMapContext } from '@/app/lib/PluginMapProvider';
 import { ShelfScanPlugin } from '@/app/lib/types/plugins';
-import { makePluginList } from '@/app/lib/utils/plugins';
-import { useContext, useEffect, useRef, useState } from 'react';
-import { FaPlus } from 'react-icons/fa6';
+import { addPlugin, makePluginList, removePlugin } from '@/app/lib/utils/plugins';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { FaMinus, FaPlus } from 'react-icons/fa6';
 
 export const PluginManager = () => {
     const [enabledPlugins, setEnabledPlugins] = useState<ShelfScanPlugin[]>([]);
@@ -23,18 +22,33 @@ export const PluginManager = () => {
         addPlugin(pluginTextAreaRef.current.value).then(loadPlugins);
     };
 
-    return <details className="text-sm">
-        <summary>Installed Plugins</summary>
-        <ul className="list list-none">
-            {enabledPlugins.map(plugin => {
-                return <li className="list-row">
-                    {plugin.name} ({plugin.type}/{plugin.location})
-                </li>;
-            })}
-        </ul>
-        <textarea ref={pluginTextAreaRef} id="plugins-new"></textarea>
-        <button className="btn" onClick={onAddPlugin}>
-            <FaPlus />
-        </button>
-    </details>;
+    return <div className="collapse collapse-arrow bg-base-100 border-base-300 text-sm">
+        <input type="checkbox" />
+        <div className="collapse-title font-semibold">Installed Plugins</div>
+        <div className="collapse-content text-xs">
+            <ul className="list-none">
+                {enabledPlugins.map(plugin => {
+                    return <li key={plugin.id} className="flex justify-between mb-1">
+                        {plugin.name} ({plugin.type}/{plugin.location})
+                        <button
+                            disabled={plugin.id.startsWith('plugin.internal')}
+                            onClick={() => {
+                                removePlugin(plugin.id).then(loadPlugins);
+                            }}
+                            className="remove-button text-gray-500 h-5 w-5 md:w-fit p-1 btn flex text-xs"
+                        >
+                            <FaMinus className="md:w-2.5 md:h-2.5" />
+                        </button>
+                    </li>;
+                })}
+            </ul>
+            <fieldset className="fieldset relative">
+                <legend className="fieldset-legend">New Plugin JSON</legend>
+                <textarea className="textarea h-24 w-full text-xs" ref={pluginTextAreaRef}></textarea>
+                <button className="btn absolute top-2 right-1 btn-xs" onClick={onAddPlugin}>
+                    <FaPlus />
+                </button>
+            </fieldset>
+        </div>
+    </div>;
 };
