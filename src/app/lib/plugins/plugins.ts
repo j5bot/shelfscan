df94447f9269f71cae252c3626fc4d233a7acf88
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2025-07-23 13:31:15 -0400

    a bit of a mess still in PM but it works

--- src/app/lib/plugins/plugins.ts
+++ src/app/lib/plugins/plugins.ts
@@ -24,7 +24,27 @@ const builtInPlugins: ShelfScanPlugin[] = [
             ],
         }
     },
+    {
+        id: 'plugin.external.BoardGameStats',
+        name: 'Board Game Stats Log Play Plugin',
+        type: 'link',
+        location: 'actions',
+        templates: {
+            game: [
+                {
+                    icon: '0 0 225 225/M34 193h162c-1.648-9.099-10.093-15.351-17-20.789-13.515-10.643-30.19-20.001-37.691-36.211-3.905-8.44-6.149-17.82-7.439-27-.469-3.332-1.653-7.538-.267-10.79 1.991-4.67 8.371-7.816 11.292-12.21 8.172-12.292 8.553-29.676.255-41.96C129.841 21.374 92.583 23.996 83.363 51 79.27 62.983 80.3 76.557 87.789 87c2.777 3.874 8.869 6.894 10.593 11.21 1.281 3.205-.34 7.579-.982 10.79-1.944 9.718-4.194 19.076-8.67 28-7.72 15.389-23.633 24.881-36.73 35.211-7.035 5.548-15.094 12.027-18 20.789Z',
+                    iconSize: 15,
+                    title: 'Log Play',
+                    template: 'https://app.bgstatsapp.com/addPlay.html?gameId={{id}}',
+                    className: 'min-w-29'
+                }
+            ],
+            version: []
+        }
+    }
 ];
+
+const builtInPluginIds = builtInPlugins.map(plugin => plugin.id);
 
 export const addPlugin = async (pluginJSON: string) => {
     const plugin = JSON.parse(pluginJSON);
@@ -78,24 +98,32 @@ export const removePlugin = async (id: string) => {
 };
 
 export const getEnabledOrDisabledPlugins = async (enabled: boolean = true): Promise<ShelfScanPlugin[]> => {
-    const pluginList = (
+    let pluginList = (
            await getSetting(enabled ? 'plugins' : 'disabledPlugins') as string[]
        ) ?? [];
+    pluginList = enabled ? builtInPluginIds.concat(pluginList) : pluginList;
     return (
         await Promise
             .all(
-                pluginList.map(pluginId => getPlugin(pluginId))
+                pluginList.map(pluginId =>
+                    builtInPluginIds.includes(pluginId) ?
+                    Promise.resolve(builtInPlugins.find(plugin => plugin.id === pluginId)) :
+                    getPlugin(pluginId))
             )
     ).filter((x: unknown) => x) as ShelfScanPlugin[];
 };
 
 export const makeEnabledOrDisabledPluginList =
-    async (enabled: boolean = true) =>
-        (enabled ? builtInPlugins : []).concat(await getEnabledOrDisabledPlugins(enabled));
+    async (enabled: boolean = true) => await getEnabledOrDisabledPlugins(enabled);
 
 export const makePluginMap = async () => {
+    const disabledPlugins = await getSetting('disabledPlugins') as string[];
     return (await makeEnabledOrDisabledPluginList(true)).reduce((acc, plugin) => {
         if (!plugin) {
+            return acc;
+        }
+        // force disabled, such as for built-in plugins which are always in the plugin list
+        if (disabledPlugins?.includes(plugin.id)) {
             return acc;
         }
         const type = acc[plugin.type] ?? {};
--- src/app/lib/types/plugins.ts
+++ src/app/lib/types/plugins.ts
@@ -18,6 +18,7 @@ export type Templates = Record<TemplateType, Template[]>;
 
 export type ShelfScanPlugin = {
     id: string;
+    defaultDisabled?: boolean;
     name?: string;
     type: 'link';
     location: 'details' | 'actions';
