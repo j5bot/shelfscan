eb972dc2a87ad8bc02b28c880c0d30a79524c1b9
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2025-07-27 20:33:41 -0400

    image cache management

---
+++ src/app/ui/ImageCacheManager.tsx
@@ -0,0 +1,35 @@
+import { clearImageCache, getImageCacheUsage } from '@/app/lib/database/cacheDatabase';
+import React, { useEffect, useState } from 'react';
+
+export const ImageCacheManager = () => {
+    const [usage, setUsage] = useState<string>();
+
+    useEffect(() => {
+        (async () => {
+            setUsage(await getImageCacheUsage());
+        })();
+    }, []);
+
+    return <div className="collapse collapse-arrow bg-base-100 border-1 border-base-300 text-sm">
+        <input type="radio" name="settings" />
+        <h3 className="collapse-title font-semibold">Image Cache</h3>
+        <div className="collapse-content text-xs">
+            <div className="p-1 flex flex-col gap-2">
+                <p className="text-balance">
+                    ShelfScan uses a local image cache to avoid re-downloading images from
+                    BoardGameGeek.  Please do not clear this cache frequently.
+                </p>
+                <p>Current estimated cache usage:</p>
+                <p>{usage}</p>
+            </div>
+            <p className="mt-2">
+                <button className="btn btn-warning" onClick={async () => {
+                    await clearImageCache();
+                    setUsage('none');
+                }}>
+                    Clear Image Cache
+                </button>
+            </p>
+        </div>
+    </div>;
+};
