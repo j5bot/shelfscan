23119c49be59df14c2b41d93fa5ecb0a394edb5a
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2026-03-29 11:29:11 -0400

    update dependencies.  patch to make undecaf package work with nextjs 16

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     allowedDevOrigins: ['192.168.0.*'],
+    serverExternalPackages: ['@undecaf/zbar-wasm'],
     images: {
         remotePatterns: [
             new URL('https://cf.geekdo-images.com/**'),
