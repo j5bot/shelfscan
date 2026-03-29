2eed4369d1923db5c5fca598b6edcf7a3d62fba7
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2026-03-29 09:42:19 -0400

    initial upgrades (need to fix url import to work with turbopack)

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,6 @@ const nextConfig: NextConfig = {
             new URL('https://gameupc.com/assets/img/**'),
         ],
     },
-    // https://nextjs.org/docs/app/api-reference/config/next-config-js/urlImports
-    experimental: {
-        urlImports: ['https://cdn.jsdelivr.net/'],
-    },
 };
 
 export default nextConfig;
