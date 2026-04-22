be4a1d18ec59144846e67d7df1ddfb172f2ec14f
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2026-04-22 19:03:14 -0400

    fix db versioning

--- src/app/lib/database/database.ts
+++ src/app/lib/database/database.ts
@@ -71,7 +71,7 @@ database.version(4).stores({
     dataforms: '++id',
 });
 
-database.version(4).stores({
+database.version(5).stores({
     settings: '++id',
     plugins: '++id',
     collections: '++id',
